285fe41f14618f2892e2fea6affd379a063058a7
JoschaRombach <153042844+JoschaRombach@users.noreply.github.com>
2025-04-25 13:12:01 +0200

    Add files via upload

---
+++ Run_application.R
@@ -0,0 +1,7 @@
+# Welcome: 
+# To run the app press the green "Run App" button in the top right corner of this panel ---------> 
+# or run the following line of code (Ctrl + Enter):
+if(!suppressWarnings(require('shiny'))) install.packages('shiny')
+shiny::runApp()
+
+# To end the app close the browser
